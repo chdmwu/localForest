20931b4fc7d9904788f9c16a8b0bd95d93cac5a5
Adam Bloniarz <adam.bloniarz@gmail.com>
2015-09-27 10:38:45 -0600

    Change to submit-script

--- submit-script.sh
+++ submit-script.sh
@@ -4,11 +4,12 @@ spark-submit \
     --class edu.berkeley.statistics.Simulations.SimulationsExecutors.ExecuteDistributedSimulation \
     --master local[8] \
     target/scala-2.10/DistributedForest-assembly-1.0.jar \
-     Friedman1 1 10000 700 1000
-#    GaussianProcess 4 50000 1000 200
+    GaussianProcess 4 20000 1000 1000 500
+#     Friedman1 3 10000 700 1000
     # Arguments are as follows
     # Simulation name
     # Number of partitions
     # Number of training points per partition
     # Number of PNNs per partition
     # Number of test points
+    # (GaussianProcess simulation only) number of random basis functions

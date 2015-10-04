c80709fdb7ae8176437d0143028ff6a09e595aec
Adam Bloniarz <adam.bloniarz@gmail.com>
2015-10-03 18:52:45 -0600

    Attempted fix by avoiding use of Breeze datastructures, didn't work

--- src/main/scala/edu/berkeley/statistics/DistributedForest/DistributedForest.scala
+++ src/main/scala/edu/berkeley/statistics/DistributedForest/DistributedForest.scala
@@ -1,5 +1,6 @@
 package edu.berkeley.statistics.DistributedForest
 
+import breeze.linalg.{DenseMatrix, DenseVector}
 import edu.berkeley.statistics.LocalModels.WeightedLinearRegression
 import edu.berkeley.statistics.SerialForest.{RandomForest, RandomForestParameters}
 import org.apache.spark.mllib.linalg.{Vector => mllibVector, Vectors}
@@ -50,33 +51,78 @@ object DistributedForest {
   }
 
   def predictWithLocalRegressionBatch(testData: IndexedSeq[mllibVector], forests: RDD[RandomForest],
-                                      numPNNsPerPartition: Int,
-                                      batchSize: Int): IndexedSeq[Double] = {
-    var batchData = testData.grouped(batchSize).toIndexedSeq //group into batches
-    batchData.flatMap((batch: IndexedSeq[mllibVector]) => {
-      val covMatsForests = forests.map((f: RandomForest) => {
-        val pnnsAndWeights = batch.map(f.getTopPNNsAndWeights(_, numPNNsPerPartition))
-        val pnnsWeightsAndTestPoint = pnnsAndWeights.zip(batch)
-        pnnsWeightsAndTestPoint.map(x =>
+                                          numPNNsPerPartition: Int,
+                                          batchSize: Int): IndexedSeq[Double] = {
+    val batchData = testData.grouped(batchSize).toIndexedSeq //group into batches
+    val predictions = Array.fill(testData.size)(0.0)
+    var batchIndex = 0
+    var currentIndex = 0
+
+    val nCols = testData(0).size + 1
+
+    while (batchIndex < batchData.length) {
+      val testDataBroadcasted = forests.context.broadcast(batchData(batchIndex))
+      val covMatsForestsRaw = forests.map((f: RandomForest) => {
+        val pnnsAndWeights = testDataBroadcasted.value.map(
+          f.getTopPNNsAndWeights(_, numPNNsPerPartition))
+        val pnnsWeightsAndTestPoint = pnnsAndWeights.zip(testDataBroadcasted.value)
+        val outputMatrices = pnnsWeightsAndTestPoint.map(x =>
           WeightedLinearRegression.getCovarianceMatrix(x._1, x._2))
+        val outputArrays: IndexedSeq[(Array[Double], Array[Double])] = outputMatrices.map{
+          case (covMat: DenseMatrix[Double], crossCovVec: DenseVector[Double]) => {
+            (covMat.valuesIterator.toArray, crossCovVec.valuesIterator.toArray)
+          }
+        }
+        outputArrays
       }).collect
-      batch.indices.map((i: Int) => {
-        val (partitionCovMats, partitionCrossCovs) = covMatsForests.map(_(i)).unzip
-        WeightedLinearRegression.getBetaHat(partitionCovMats, partitionCrossCovs)(0)
+
+      val covMatsForests = covMatsForestsRaw.map(_.map{
+        case (covMatRaw: Array[Double], crossCovVecRaw: Array[Double]) => {
+          (new DenseMatrix[Double](nCols, nCols, covMatRaw),
+              new DenseVector[Double](crossCovVecRaw))
+        }
       })
-    })
+
+      val predictionsBatch: IndexedSeq[Double] = batchData(batchIndex).indices.map((i: Int) => {
+        val (partitionCovMats, partitionCrossCovs) = covMatsForests.map(_(i)).unzip
+        val betaHat: DenseVector[Double] =
+          WeightedLinearRegression.getBetaHat(partitionCovMats, partitionCrossCovs)
+        betaHat(0)
+      })
+      // Fill the predictions
+      // Ugh
+      (currentIndex until (currentIndex + batchSize)).foreach(i => {
+        predictions(i) = predictionsBatch(i - currentIndex)
+      })
+      batchIndex += 1
+      currentIndex += batchSize
+    }
+
+    // Is this terrible?
+    predictions
   }
 
   def predictWithNaiveAverageBatch(testData: IndexedSeq[mllibVector],
-                                   forests: RDD[RandomForest], batchSize: Int): IndexedSeq[Double] = {
+                                       forests: RDD[RandomForest], batchSize: Int): IndexedSeq[Double] = {
     var batchData = testData.grouped(batchSize).toIndexedSeq
-    batchData.flatMap(batch => {
+    val predictions = Array.fill(testData.size)(0.0)
+    var batchIndex = 0
+    var currentIndex = 0
+
+    while (batchIndex < batchData.length) {
+      val testDataBroadcasted = forests.context.broadcast(batchData(batchIndex))
       val forestPreds = forests.map(f => {
-        batch.map(f.predict(_))
+        testDataBroadcasted.value.map(f.predict(_))
       }).collect
-      batch.indices.map(i => {
+      val predictionsBatch = testDataBroadcasted.value.indices.map(i => {
         forestPreds.map(_(i)).sum / forestPreds.size
       })
-    })
+      (currentIndex until (currentIndex + batchSize)).foreach(i => {
+        predictions(i) = predictionsBatch(i - currentIndex)
+      })
+      currentIndex = currentIndex + batchSize
+      batchIndex = batchIndex + 1
+    }
+    predictions
   }
 }
